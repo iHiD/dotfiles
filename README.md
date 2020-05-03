497baa0af4e0df1e1ded3faf8c20c006f786a0e5
Jeremy Walker <iHiD@jeremys-air.mynet>
2020-05-03 15:13:52 +0100

    Add font

--- README.md
+++ README.md
@@ -1,10 +1,49 @@
+## Chrome
+
+Download, login. Get the order of accounts right.
+
+## 1Password
+
+Download, configure. Phone required.
+
 ## Homebrew
 
 Firstly, we need Homebrew to manage dependencies. 
 
 In the dotfiles we have a Brewfile. We need to install brew, install git, clone the dotfiles repo, then run the brewfile.
 
-- Install homebrew (https://brew.sh/)
+- Install homebrew: https://brew.sh/
 - `brew install git`
 - `git clone https://github.com/iHiD/dotfiles.git ~/.dotfiles`
+- `ln -s ~/.dotfiles/files/Brewfile ~/Brewfile`
+-  `cd ~ && brew bundle`
 
+## Terminal
+
+Load iHiD terminal from dotfiles:/files
+
+## Fish
+
+Fish will be installed via Homebrew. Run these to make it default:
+- `echo /usr/local/bin/fish | sudo tee -a /etc/shells `
+- `chsh -s /usr/local/bin/fish`
+- `ln ~/.dotfiles/files/config.fish ~/.config/fish/config.fish`
+
+Install fisher:
+- `curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish`
+- `ln -s ~/.dotfiles/files/fishfile ~/.config/fish/fishfile`
+- `fish -c fisher`
+
+Set correct colours for ls:
+`set -Ux LSCOLORS gxfxbEaEBxxEhEhBaDaCaD`
+
+## Alfred
+
+Download: https://www.alfredapp.com/
+
+## Visual Studio Code
+
+Install from https://code.visualstudio.com/
+
+Copy settings:W
+`ln -s  ~/.dotfiles/files/code-settings.json ~/Library/Application\ Support/Code/User/settings.json`
--- files/config.fish
+++ files/config.fish
@@ -1,25 +1,25 @@
-$ set -U fish_color_normal normal
-$ set -U fish_color_command 00ff00
-$ set -U fish_color_quote 999900
-$ set -U fish_color_redirection 00afff
-$ set -U fish_color_end 009900
-$ set -U fish_color_error ff8787
-$ set -U fish_color_param 00afff
-$ set -U fish_color_comment 00af00
-$ set -U fish_color_match normal
-$ set -U fish_color_selection c0c0c0
-$ set -U fish_color_search_match ffff00
-$ set -U fish_color_history_current normal
-$ set -U fish_color_operator 00a6b2
-$ set -U fish_color_escape 00a6b2
-$ set -U fish_color_cwd 008000
-$ set -U fish_color_cwd_root 800000
-$ set -U fish_color_valid_path normal
-$ set -U fish_color_autosuggestion 555
-$ set -U fish_color_user 00ff00
-$ set -U fish_color_host normal
-$ set -U fish_color_cancel normal
-$ set -U fish_pager_color_completion normal
-$ set -U fish_pager_color_description B3A06D yellow
-$ set -U fish_pager_color_prefix white --bold --underline
-$ set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_normal normal
+set -U fish_color_command 00ff00
+set -U fish_color_quote ffd700
+set -U fish_color_redirection 00afff
+set -U fish_color_end 009900
+set -U fish_color_error ff8787
+set -U fish_color_param 00afff
+set -U fish_color_comment 00af00
+set -U fish_color_match normal
+set -U fish_color_selection c0c0c0
+set -U fish_color_search_match ffff00
+set -U fish_color_history_current normal
+set -U fish_color_operator 00a6b2
+set -U fish_color_escape 00a6b2
+set -U fish_color_cwd 008000
+set -U fish_color_cwd_root 800000
+set -U fish_color_valid_path normal
+set -U fish_color_autosuggestion 555
+set -U fish_color_user 00ff00
+set -U fish_color_host normal
+set -U fish_color_cancel normal
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description B3A06D yellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
